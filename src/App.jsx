05350d039605c3494a71a296d156d849349dc33a
Vladimir Korolev <vovkaemerald2000@gmail.com>
2025-02-23 13:16:09 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -5,29 +6,25 @@ import './App.css'
 function App() {
   const year = new Date().getFullYear()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {return year}}>
-          Current year: {year}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return React.createElement(
+    React.Fragment, null,
+    React.createElement("div", null,
+      React.createElement("a", {href: "https://vite.dev", target: "_blank"},
+        React.createElement("img", {src: viteLogo, className: "logo", alt: "Vite logo"})
+      ),
+      React.createElement("a", {href: "https://react.dev", target: "_blank"},
+        React.createElement("img", {src: reactLogo, className: "logo react", alt: "React logo"})
+      ),
+    ),
+    React.createElement("h1", null, "Vite + React"),
+    React.createElement("div", {className: "card"},
+      React.createElement("button", {onClick: () => year}, `Current year: ${year}`),
+      React.createElement("p", null, "Edit ",
+        React.createElement("code", null, "src/App.jsx"),
+        "and save to test HMR"
+      )
+    ),
+    React.createElement("p", {className: "read-the-docs"}, "Click on the Vite and React logos to learn more")
   )
 }
 
